c50539fbdfeebd7e184e0b3b35482ae400c42172
sandaniUma <sanuma276@gmail.com>
2020-09-21 19:06:51 +0530

    update-faceid
    update-faceid

---
+++ NIBM_Covid19/FaceIDViewController.swift
@@ -0,0 +1,59 @@
+//
+//  FaceIDViewController.swift
+//  
+//
+//  Created by Digital-09 on 9/21/20.
+//
+
+import UIKit
+import LocalAuthentication
+
+class FaceIDViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnDisplay(_ sender: UIButton) {
+        
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [weak self] success, authenticationError in
+                
+                DispatchQueue.main.async {
+                    if success {
+                        let ac = UIAlertController(title: "Authentication success", message: "Well Done", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "Happy", style: .default))
+                        self?.present(ac, animated: true)
+                    } else {
+                        let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
